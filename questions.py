fb5c9118b122637615e3094ffecc3737da85f187
Chirag Sarda <chirag@Chirags-MacBook-Air.local>
2023-09-07 04:29:06 +0530

    Structure of app made

---
+++ questions.py
@@ -0,0 +1,113 @@
+questions = [
+    {
+        "question": "What is the capital of France?",
+        "options": ["Paris", "Berlin", "Madrid", "London"],
+        "correct_option": "Paris",
+    },
+    {
+        "question": "What is the largest planet in our solar system?",
+        "options": ["Earth", "Mars", "Jupiter", "Saturn"],
+        "correct_option": "Jupiter",
+    },
+    {
+        "question": "Which gas do plants absorb from the atmosphere?",
+        "options": ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+        "correct_option": "Carbon Dioxide",
+    },
+    
+    {
+        "question": "What is the largest mammal in the world?",
+        "options": ["African Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+        "correct_option": "Blue Whale",
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Venus", "Mars", "Jupiter", "Mercury"],
+        "correct_option": "Mars",
+    },
+    {
+        "question": "Who wrote the famous play 'Romeo and Juliet'?",
+        "options": ["William Shakespeare", "Jane Austen", "Charles Dickens", "Leo Tolstoy"],
+        "correct_option": "William Shakespeare",
+    },
+    {
+        "question": "What is the chemical symbol for the element gold?",
+        "options": ["Go", "Gd", "Ge", "Au"],
+        "correct_option": "Au",
+    },
+    {
+        "question": "Which gas makes up the majority of Earth's atmosphere?",
+        "options": ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+        "correct_option": "Nitrogen",
+    },
+    {
+        "question": "Which continent is the Sahara Desert located in?",
+        "options": ["Africa", "Asia", "North America", "South America"],
+        "correct_option": "Africa",
+    },
+    {
+        "question": "What is the largest ocean in the world?",
+        "options": ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        "correct_option": "Pacific Ocean",
+    },
+    {
+        "question": "Who is known as the 'Father of Modern Physics'?",
+        "options": ["Albert Einstein", "Isaac Newton", "Galileo Galilei", "Stephen Hawking"],
+        "correct_option": "Albert Einstein",
+    },
+    {
+        "question": "Which gas do humans exhale when they breathe out?",
+        "options": ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+        "correct_option": "Carbon Dioxide",
+    },
+    {
+        "question": "Which famous scientist is credited with the discovery of gravity?",
+        "options": ["Albert Einstein", "Isaac Newton", "Galileo Galilei", "Stephen Hawking"],
+        "correct_option": "Isaac Newton",
+    },
+    {
+        "question": "In which year did Christopher Columbus reach the Americas?",
+        "options": ["1492", "1607", "1776", "1812"],
+        "correct_option": "1492",
+    },
+    {
+        "question": "What is the national flower of Japan?",
+        "options": ["Cherry Blossom", "Rose", "Tulip", "Daisy"],
+        "correct_option": "Cherry Blossom",
+    },
+    {
+        "question": "Which country is known as the Land of the Rising Sun?",
+        "options": ["China", "South Korea", "Japan", "Vietnam"],
+        "correct_option": "Japan",
+    },
+    {
+        "question": "Who is the author of 'To Kill a Mockingbird'?",
+        "options": ["George Orwell", "Mark Twain", "Harper Lee", "F. Scott Fitzgerald"],
+        "correct_option": "Harper Lee",
+    },
+    {
+        "question": "Which planet is often referred to as Earth's twin?",
+        "options": ["Mars", "Venus", "Jupiter", "Saturn"],
+        "correct_option": "Venus",
+    },
+    {
+        "question": "What is the largest desert in the world?",
+        "options": ["Sahara Desert", "Arabian Desert", "Gobi Desert", "Antarctica"],
+        "correct_option": "Antarctica",
+    },
+    {
+        "question": "What is the capital of Australia?",
+        "options": ["Sydney", "Melbourne", "Canberra", "Perth"],
+        "correct_option": "Canberra",
+    },
+    {
+        "question": "Which famous scientist is known for the theory of evolution by natural selection?",
+        "options": ["Charles Darwin", "Albert Einstein", "Isaac Newton", "Marie Curie"],
+        "correct_option": "Charles Darwin",
+    },
+    {
+        "question": "What is the currency of Brazil?",
+        "options": ["Peso", "Dollar", "Euro", "Real"],
+        "correct_option": "Real",
+    },
+]
